fca9c6a6c345ce519f1c1cae809b196cd117dba3
Michael Klatt <mdklatt@alumni.ou.edu>
2023-05-29 23:43:05 -0500

    Added stub implementation of pqinsert/test_pqinsert_stdin

---
+++ pqinsert/test_pqinsert_stdin.c
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2023, University Corporation for Atmospheric Research
+ * See the file COPYRIGHT in the top-level source-directory for copying and
+ * redistribution conditions.
+ *
+ * @author Michael Klatt
+ * @file   Test execution of the `pqinsert` command with input from STDIN
+ */
+#include <stdlib.h>
+
+
+/**
+ *
+ * @param argc  command line argument count
+ * @param argv  command line argument values
+ * @return      EXIT_SUCCESS if all tests were successful
+ */
+int main(int argc, char* argv[])
+{
+    return EXIT_SUCCESS;
+}
